653abff1fa3203151411c6a662064f6e8de09460
RAHUL SINGH RAWAT <121442029+RahulRawatdevops@users.noreply.github.com>
2023-12-28 17:23:18 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -12,3 +12,15 @@ def divide(x, y):
     return x / y
   else:
     return "Error:Division by zero"
+
+def calculator():
+  print("Simple Python Calculator")
+  print("Operation")
+  print("1. Addition (+)")
+  print("2. Subtraction (-)")
+  print("3. Multiplication (*)"
+  print("4. Division (/)")
+
+  choice = input("Enter operation number (1/2/3/4): ")
+  
+        
